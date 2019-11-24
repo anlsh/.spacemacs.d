d8663c61e84f745538df073b8009d943048d2ba3
Anish Moorthy <anlsh@protonmail.com>
2019-11-24 02:01:22 -0500

    Get erc working perfectly

--- init.el
+++ init.el
@@ -425,7 +425,58 @@ you should place your code here."
   (add-hook 'before-save-hook 'delete-trailing-whitespace)
   (use-package whitespace
     :hook (before-save . delete-trailing-whitespace)
-    :config (global-whitespace-mode)))
+    :config (global-whitespace-mode))
+
+  (use-package erc
+    ;; Sourced from
+    ;; https://www.reddit.com/r/emacs/comments/8ml6na/tip_how_to_make_erc_fun_to_use/
+    ;; Which is actually the excellent git repo
+    ;; https://github.com/rememberYou/.emacs.d/blob/master/config.org#irc
+    :preface
+    (defun my/erc-notify (nickname message)
+      "Displays a notification message for ERC."
+      (let* ((channel (buffer-name))
+             (nick (erc-hl-nicks-trim-irc-nick nickname))
+             (title (if (string-match-p (concat "^" nickname) channel)
+                        nick
+                      (concat nick " (" channel ")")))
+             (msg (s-trim (s-collapse-whitespace message))))
+        (alert (concat nick ": " msg) :title title)))
+
+    (defun my/erc-preprocess (string)
+      "Avoids channel flooding."
+      (setq str
+            (string-trim
+             (replace-regexp-in-string "\n+" " " str))))
+
+    :hook ((ercn-notify . my/erc-notify)
+           (erc-send-pre . my/erc-preprocess))
+
+    :custom
+    (erc-autojoin-timing 'ident)
+    ;; TODO These lines create some sort of margin, no space on laptop screens :|
+    ;; (erc-fill-function 'erc-fill-static)
+    ;; (erc-fill-static-center 22)
+    (erc-hide-list '("JOIN" "PART" "QUIT"))
+    (erc-lurker-hide-list '("JOIN" "PART" "QUIT"))
+    (erc-lurker-threshold-time 43200)
+    (erc-prompt-for-password nil)
+    (erc-prompt-for-nickserv-password nil)
+    (erc-server-reconnect-attempts 5)
+    (erc-server-reconnect-timeout 3)
+    (erc-nick "anlsh")
+    (erc-server-list '(("irc.freenode.net"
+                        :port "6697"
+                        :ssl t
+                        :nick "anlsh")))
+    (erc-track-exclude-types '("JOIN" "MODE" "NICK" "PART" "QUIT"
+                               "324" "329" "332" "333" "353" "477"))
+    :config
+    (add-to-list 'erc-modules 'notifications)
+    (add-to-list 'erc-modules 'spelling)
+    (erc-services-enable)
+    (erc-update-modules))
+  )
 
 
 ;; Do not write anything past this comment. This is where Emacs will
@@ -457,6 +508,9 @@ This function is called at the very end of Spacemacs initialization."
    ;; If you edit it by hand, you could mess it up, so be careful.
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
+   '(erc-modules
+     (quote
+      (completion log notify notifications image youtube spelling hl-nicks netsplit fill button match track readonly networks ring autojoin noncommands irccontrols move-to-prompt stamp menu list)))
    '(org-agenda-files (quote ("~/org")))
    '(package-selected-packages
      (quote
